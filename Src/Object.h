5b0e96c95ebd1f74f0cca87b8d975f0eff6239ba
juandigilio <juanignaciodigilio@gmail.com>
2024-01-16 10:05:44 -0300

    Program reestarted

--- Src/Object.h
+++ Src/Object.h
@@ -11,18 +11,89 @@ struct Date
 	int year;
 };
 
+enum class Type
+{
+	//clothes
+
+	GuardaPolvo,
+	Camisa,
+	Chaqueta, 
+	Pantalon,
+	Delantal, 
+	Campera,
+	Chaleco,
+	Buzo,
+	Remera,
+	ZapatosSeguridad,
+	Botas,
+	Cascos,
+
+	//tools
+
+	LlaveCombinadaMetrica,
+	LlaveCombinadaImperial,
+	LLaveFijaMetrica,
+	LLaveFijaImperial,
+	LLaveCanio,
+	LLaveFrancesa,
+	Masa,
+	Martillo,
+	JuegoAllen,
+	JuegoThorx,
+	DestornilladorPhillips,
+	DestornilladorPlano,
+	Multimetro,
+	PinzaAmperometrica,
+	CintaMetrica,
+	GuanteTela,
+	GuanteCuero,
+	SoldadoraElectrica,
+	SoldadorEstanio,
+	Taladro,
+	Percutor,
+	Rotopercutor,
+	Sensitiva,
+	AmoladoraGrande,
+	AmoladoraChica,
+	ArcoSierra,
+	Tenaza,
+	Pinza,
+	Alicate,
+	BuscaPolo,
+	Tijeras,
+	MascaraSoldar,
+	Antiparra,
+	PinzaFuerza,
+	LLaveDeTubo,
+	Tubo,
+	CortaHierro,
+	Punta,
+	MechaHierro,
+	MechaWidia,
+	Electrodo,
+	ElectrodoInoxidable,
+	DiscoCorte,
+	DiscoDesbaste,
+	DiscoSensitiva,
+	DiscoFlap,
+	TaladroDeMesa,
+	PiedraBanco
+};
+
 class Object
 {
 protected:
 
 	Date date;
+	Type type;
 
 
 public:
 
-	Object(Date date) { this->date = date; }
-	virtual ~Object() {}
+	Object(Date date, Type type) { this->date = date; this->type = type; }
+	~Object() {}
 
 	Date GetDate() { return date; }
+	Type GetType() { return type; }
 };
 
